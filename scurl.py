dc561ecbc99b778534653743ea013a77a2140017
jfarman <jfarman@stanford.edu>
2016-03-07 01:26:50 -0800

    Cleaned up comments

--- scurl.py
+++ scurl.py
@@ -7,24 +7,11 @@ from urlparse import urlparse
 
 # https://curl.haxx.se/docs/manpage.html
 
-# [--tlsv1] ==> (SSL) Forces curl to use TLS version 1.x when negotiating with a remote TLS server.
-# You can use options --tlsv1.0, --tlsv1.1, and --tlsv1.2 to control the TLS version more precisely,
-# provided that the SSL backend in use supports such a level of control).
-
-# The behavior of what happens if a callback functions returns False depends on the verification method set.
-# If SSL.VERIFY_NONE was used then the verification chain is not followed.
-# if SSL.VERIFY_PEER was used then a callback function returning False will raise an OpenSSL.SSL.Error exception.
-def callback(conn, cert, errno, depth, result):
-    if depth == 0 and (errno == 9 or errno == 10):
-        return False # or raise Exception("Certificate not yet valid or expired")
-    return True
-
 def initOptionParser():
 	parser.add_option("--tlsv1.0", action="store_const", const="tlsv1.0", dest="protocol", default="--tlsv1.2", help="todo")
 	parser.add_option("--tlsv1.1", action="store_const", const="tlsv1.1", dest="protocol", default="--tlsv1.2", help="todo")
 	parser.add_option("--tlsv1.2", action="store_const", const="tlsv1.2", dest="protocol", default="--tlsv1.2", help="todo")
-	parser.add_option("--sslv3", action="store_const", const="sslv3", dest="protocol", default="--tlsv1.2", help="todo")
-	parser.add_option("--3", action="store_const", const="3", dest="protocol", default="--tlsv1.2", help="todo")
+	parser.add_option("--sslv3", "-3", action="store_const", const="sslv3", dest="protocol", default="--tlsv1.2", help="todo")
 
 	# --crlfile <file>
 	# (HTTPS/FTPS) Provide a file using PEM format with a Certificate Revocation List that may specify peer certificates that
@@ -61,6 +48,16 @@ def initOptionParser():
 def request(host, path):
     return '''GET %s\r\nHTTP/1.0\r\nHost: %s\r\nConnection: close''' % (path, host)
 
+# The behavior when callback returns false depends on the verification method set.
+# If SSL.VERIFY_NONE was used then the verification chain is not followed.
+# if SSL.VERIFY_PEER was used then a callback function returning False will raise an OpenSSL.SSL.Error exception.
+def callback(conn, cert, errno, depth, result):
+    if depth == 0 and (errno == 9 or errno == 10):
+        return False # or raise Exception("Certificate not yet valid or expired")
+    return True
+
+protocols = {'tlsv1.0': SSL.TLSv1_METHOD, 'tlsv1.1': SSL.TLSv1_1_METHOD, 'tlsv1.2': SSL.TLSv1_2_METHOD, 'sslv3' : SSL.SSLv3_METHOD}
+
 parser = OptionParser()
 initOptionParser()
 (options, args) = parser.parse_args()
@@ -70,42 +67,38 @@ print options 	# also: print args
 url = urlparse(args[0])
 print url
 
-if url.scheme == 'https':
-	print "yay"
-else:
+if url.scheme != 'https':
 	sys.exit('Error!')
 
 host = 'www.google.com'
 path = '/doodles/about'
 
-# TODO: URL PARSING
-# TODO: STRING DELIMITING FOR CIPHERS LIST
+
+# TODO: STRING DELIMITING FOR CIPHERS LIST; set_cipher_list()
 # TODO: pinned public key option override, check for none
+# TODO: implement SNI
+# TODO: Figure out what happens when SSL.SysCallError occurs from a recv() call [???]
 
-context = SSL.Context(SSL.TLSv1_METHOD)
-# context.set_options(SSL.OP_NO_SSLv2)					# ?? DO I NEED TO DO THIS?
-context.set_verify(SSL.VERIFY_NONE, callback)			# ?? WHICH VERFICATION MODE (sets verification mode & callback fn to call when verifying)
-# context.load_verify_locations(ca_file, ca_path)		# ?? WHAT IS GOING ON HERE
+context = SSL.Context(protocols[options.protocol])
 
-s = socket()											# ?? (socket.AF_INET, socket.SOCK_STREAM)
-# s.settimeout(5)										# ?? do i need to do this? try catch
+# [???] Are there any options I need to set? What does load_verify_locations mean?
+# context.set_options(SSL.OP_NO_SSLv2)
+# context.load_verify_locations(ca_file, ca_path)
+
+# [???] If no server certificate is sent, because an anonymous cipher is used, SSL_VERIFY_PEER is ignored.
+context.set_verify(SSL.VERIFY_PEER, callback)			
+
+# [???] Do I need to do <<s.settimeout(5)>>? What about socket arguments, ie <<socket(socket.AF_INET, socket.SOCK_STREAM)>>?
+# [???] What about try catch for socket connection (or for handshake), ie <<except SSL.WantReadError>>?
+# [???] Should I specify the default port (ie port 443)?
+
+# [???] For TCP, is socket.SOCK_STREAM necessary?
+s = socket()
 connection = SSL.Connection(context, s)
-# connection.connect((ip_addr, port))					# ?? where do you get IP ADDRESS AND PORT ("www.facebook.com",443) ??
 connection.connect((host,443))
-connection.do_handshake()								# ?? TRY CATCH FOR HANDSHAKE
+connection.do_handshake()
 
 connection.sendall(request(host, path))
+# [???] What terminates this loop?
 print connection.read(1024)
 
-# set_cipher_list
-# SSL.SysCallError occurs from a recv() call
-
-#connection.sendall(req)
-#print connection.read(1024)
-
-# TRY CATCH FOR HANDSHAKE
-# try: 
-#	connection.do_handshake()
-# except SSL.WantReadError:
-# print "Timeout"
-# quit()
