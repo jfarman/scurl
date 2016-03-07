ece4fa04e037fb1b78047ee52eb390810c422e64
jfarman <jfarman@stanford.edu>
2016-03-06 23:24:53 -0800

    Added basic command line parsing functionality using optparser

--- scurl.py
+++ scurl.py
@@ -1,19 +1,54 @@
 import re
 from OpenSSL import SSL
 from socket import socket
+from optparse import OptionParser
 
 # https://curl.haxx.se/docs/manpage.html
 
-# [--tlsv1] ==> (SSL) Forces curl to use TLS version 1.x when negotiating with a remote TLS server. You can use options --tlsv1.0, --tlsv1.1, and --tlsv1.2 to control the TLS version more precisely (if the SSL backend in use supports such a level of control).
+# [--tlsv1] ==> (SSL) Forces curl to use TLS version 1.x when negotiating with a remote TLS server.
+# You can use options --tlsv1.0, --tlsv1.1, and --tlsv1.2 to control the TLS version more precisely,
+# provided that the SSL backend in use supports such a level of control).
 
-# The behavior of what happens if a callback functions returns False depends on the verification method set: if SSL.VERIFY_NONE was used then the verification chain is not followed but if SSL.VERIFY_PEER was used then a callback function returning False will raise an OpenSSL.SSL.Error exception.
+# The behavior of what happens if a callback functions returns False depends on the verification method set.
+# If SSL.VERIFY_NONE was used then the verification chain is not followed.
+# if SSL.VERIFY_PEER was used then a callback function returning False will raise an OpenSSL.SSL.Error exception.
 def callback(conn, cert, errno, depth, result):
     if depth == 0 and (errno == 9 or errno == 10):
         return False # or raise Exception("Certificate not yet valid or expired")
     return True
 
+def initOptionParser():
+	parser.add_option("--tlsv1.0", action="store_const", const="tlsv1.0", dest="protocol", default="--tlsv1.2", help="todo")
+	parser.add_option("--tlsv1.1", action="store_const", const="tlsv1.1", dest="protocol", default="--tlsv1.2", help="todo")
+	parser.add_option("--tlsv1.2", action="store_const", const="tlsv1.2", dest="protocol", default="--tlsv1.2", help="todo")
+	parser.add_option("--sslv3", action="store_const", const="sslv3", dest="protocol", default="--tlsv1.2", help="todo")
+	parser.add_option("--3", action="store_const", const="3", dest="protocol", default="--tlsv1.2", help="todo")
+
+	# --crlfile <file>
+	# (HTTPS/FTPS) Provide a file using PEM format with a Certificate Revocation List that may specify peer certificates that
+	# are to be considered revoked. If this option is used several times, the last one will be used.
+	parser.add_option("--crlfile", action="store", dest="crlfile")
+
+	# --cacert <CA certificate>
+	# (SSL) Tells curl to use the specified certificate file to verify the peer. The file may contain multiple CA certificates.
+	# The certificate(s) must be in PEM format. Normally curl is built to use a default file for this, so this option is
+	# typically used to alter that default file.
+	parser.add_option("--cacert", action="store", dest="cacert")
+
+	parser.add_option("--allow-state-certs", action="store", dest="expiration")
+
+	return
+
 def request(host, path):
     return '''GET %s\r\nHTTP/1.0\r\nHost: %s\r\nConnection: close''' % (path, host)
+
+
+parser = OptionParser()
+initOptionParser()
+(options, args) = parser.parse_args()
+
+print options
+print args[0]
 
 host = 'www.google.com'
 path = '/doodles/about'
